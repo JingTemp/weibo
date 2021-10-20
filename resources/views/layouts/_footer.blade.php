5ca717ad0ee485286d2964a12537f85835c807fd
JingTemp <chiyiyoudao@gmail.com>
2021-10-20 02:05:17 +0000

    feat: add header and fotter blade

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+  <img class="brand-icon" src="http://t13.baidu.com/it/u=1557293032,753455875&fm=224&app=112&f=JPEG?w=500&h=493&s=D8C8B3555C0268C006CC81D60100C033">
+  刻意练习，每日精进
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
